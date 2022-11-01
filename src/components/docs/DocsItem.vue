ed466021fc30ab5a1afcc160820313b23d812502
Заур <zaurmag411@gmail.com>
2022-11-01 15:54:07 +0300

    Feat: animate dropdown

--- src/components/docs/DocsItem.vue
+++ src/components/docs/DocsItem.vue
@@ -1,16 +1,27 @@
 <template>
-  <div v-if="docItem" :class="['docs__item', {'docs__item--cat': docItem.type === 'cat'}, classList]">
-    <app-item :item="docItem" />
+  <div
+      v-if="docItem"
+      :class="[
+        'docs__item',
+        {'docs__item--cat': docItem.type === 'cat'},
+        {'is-active': dropdown},
+        classList
+      ]"
+  >
+    <app-item :item="docItem" @click="dropdownHandler" />
 
-    <div class="docs__items" v-if="docItem.items.length">
+    <div
+        class="docs__items"
+        v-if="docItem?.items?.length"
+        :style="`height: ${nestedItemsTotalHeight}px`"
+        ref="nestedItems"
+    >
       <div
           class="docs__item"
           v-for="item in docItem.items"
           :key="item.id"
       >
-        <div class="docs__item-row">
-          <app-item :item="item" />
-        </div>
+        <app-item :item="item" />
       </div>
     </div>
   </div>
@@ -18,7 +29,7 @@
 
 <script setup>
 import AppItem from '../AppItem.vue'
-import { defineProps } from 'vue'
+import { ref, onMounted } from 'vue'
 
 defineProps({
   docItem: {
@@ -31,4 +42,28 @@ defineProps({
   }
 })
 
+const dropdown = ref(false)
+const nestedItems = ref(null)
+const nestedItemsTotalHeight = ref(0)
+const dropdownHandler = ref(null);
+
+onMounted(() => {
+  if (nestedItems.value) {
+    const docItems = Array.from(nestedItems.value.querySelectorAll('.docs__item'))
+
+    dropdownHandler.value = () => {
+      if (!dropdown.value) {
+        nestedItemsTotalHeight.value = docItems.reduce((acc, item) => {
+          acc += item.clientHeight
+
+          return acc - 1
+        }, 0)
+      } else {
+        nestedItemsTotalHeight.value = 0
+      }
+
+      dropdown.value = !dropdown.value
+    }
+  }
+})
 </script>
